6e3046ba9b9cce41dd16fddfda82f16af40c8e2f
patrickodea <po39dea@gmail.com>
2023-08-18 17:38:10 -0400

    Setting up save buttom click event listener

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,7 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+$(document).ready(function () {
 
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
@@ -10,8 +11,14 @@
     // useful when saving the description in local storage?
  // Function to handle the click event on the save button
 
+ function SaveButtonClick(event) {
+  var timeBlockId = event.target.parentElement.id;
+  var userInput = event.target.parentElement.querySelector('.description').value; 
+
   // Save the user input in local storage using the time-block's ID as the key
- 
+  
+  localStorage.setItem(timeBlockId, userInput);
+} 
 
 // Add click event listeners to all the save buttons
 
